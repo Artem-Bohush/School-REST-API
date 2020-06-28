24a40541283abfa6502506d6fdbf26ec09125691
Artem Bohush <art.bohush@gmail.com>
2020-06-28 16:49:17 +0300

    implemented connection to mongodb atlas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, './config/.env') });
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Atlas successfully connected!'))
+  .catch((err) => console.log(err));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server was running on the ${PORT} port`));
+app.listen(PORT, () => console.log(`Server was running on the ${PORT} port ...`));
