61d0d985e99198bc75df8bd00fd7c26f954af7c5
Artem Bohush <art.bohush@gmail.com>
2020-07-02 19:32:16 +0300

    described lesson, teacher, group and student model

---
+++ src/models/Lesson.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const lessonSchema = new Schema({
+  topic: {
+    type: String,
+    required: true,
+  },
+  teacher: {
+    ref: 'teachers',
+    type: Schema.Types.ObjectId,
+  },
+  group: {
+    ref: 'groups',
+    type: Schema.Types.ObjectId,
+  },
+  classroom: {
+    type: String,
+    required: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+  },
+  school: {
+    ref: 'schools',
+    type: Schema.Types.ObjectId,
+  },
+});
+
+module.exports = mongoose.model('lessons', lessonSchema);
