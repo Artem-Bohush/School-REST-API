d9a0f39fad832b877b7c48995d2bcbb67fd11158
Artem Bohush <art.bohush@gmail.com>
2020-07-04 10:37:08 +0300

    implemented Group controller

--- src/controllers/group.js
+++ src/controllers/group.js
@@ -1,19 +1,49 @@
-module.exports.getAll = async (req, res) => {
-  res.status(200).json({ message: 'Group, getAll, staging response!' });
-};
+const Group = require('../models/Group');
+const errorHandler = require('../helpers/errorHandler');
 
-module.exports.getById = async (req, res) => {
-  res.status(200).json({ message: 'Group, getById, staging response!' });
+module.exports.getAll = async (req, res) => {
+  try {
+    const groups = await Group.find({ school: req.user.id });
+    res.status(200).json(groups);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.remove = async (req, res) => {
-  res.status(200).json({ message: 'Group, remove, staging response!' });
+  try {
+    const result = await Group.deleteOne({ _id: req.params.id });
+    if (result.deletedCount) {
+      res.status(200).json({ success: true, message: 'Group successfully deleted!' });
+    } else {
+      res.status(404).json({ success: false, message: 'Such a group was not found in the database!' });
+    }
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.create = async (req, res) => {
-  res.status(200).json({ message: 'Group, create, staging response!' });
+  try {
+    const group = await new Group({
+      name: req.body.name,
+      school: req.user.id,
+    }).save();
+    res.status(201).json(group);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.update = async (req, res) => {
-  res.status(200).json({ message: 'Group, update, staging response!' });
+  try {
+    const updatedGroup = await Group.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true },
+    );
+    res.status(200).json(updatedGroup);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
