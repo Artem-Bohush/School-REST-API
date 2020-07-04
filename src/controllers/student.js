e183ab49229fa041e834e3207332b6ca4f0e199f
Artem Bohush <art.bohush@gmail.com>
2020-07-04 10:19:20 +0300

    implemented Student controller

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -1,19 +1,63 @@
+const Student = require('../models/Student');
+const errorHandler = require('../helpers/errorHandler');
+
 module.exports.getAll = async (req, res) => {
-  res.status(200).json({ message: 'Student, getAll, staging response!' });
+  try {
+    const students = await Student.find({ school: req.user.id });
+    res.status(200).json(students);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.getByGroupId = async (req, res) => {
-  res.status(200).json({ message: 'Student, getByGroupId, staging response!' });
+  try {
+    const students = await Student.find({
+      school: req.user.id,
+      group: req.params.groupId,
+    });
+    res.status(200).json(students);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.remove = async (req, res) => {
-  res.status(200).json({ message: 'Student, remove, staging response!' });
+  try {
+    const result = await Student.deleteOne({ _id: req.params.id });
+    if (result.deletedCount) {
+      res.status(200).json({ success: true, message: 'Studdent successfully deleted!' });
+    } else {
+      res.status(404).json({ success: false, message: 'Such a student was not found in the database!' });
+    }
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.create = async (req, res) => {
-  res.status(200).json({ message: 'Student, create, staging response!' });
+  try {
+    const student = await new Student({
+      name: req.body.name,
+      surname: req.body.surname,
+      group: req.body.group,
+      school: req.user.id,
+    }).save();
+    res.status(201).json(student);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
 module.exports.update = async (req, res) => {
-  res.status(200).json({ message: 'Student, update, staging response!' });
+  try {
+    const updatedStudent = await Student.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true },
+    );
+    res.status(200).json(updatedStudent);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
