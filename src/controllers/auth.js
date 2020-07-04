bb085e892fb186b832be4edf94966b369a59ba38
Artem Bohush <art.bohush@gmail.com>
2020-07-04 19:54:04 +0300

    implemented account deleting and cascade deleting

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const School = require('../models/School');
+const Group = require('../models/Group');
+const Teacher = require('../models/Teacher');
+const Student = require('../models/Student');
+const Lesson = require('../models/Lesson');
+const Schedule = require('../models/Schedule');
 const errorHandler = require('../helpers/errorHandler');
 
 module.exports.login = async (req, res) => {
@@ -45,6 +50,20 @@ module.exports.register = async (req, res) => {
       await school.save();
       res.status(201).json(school);
     }
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
+module.exports.remove = async (req, res) => {
+  try {
+    await School.deleteOne({ _id: req.user.id });
+    await Schedule.deleteMany({ school: req.user.id });
+    await Teacher.deleteMany({ school: req.user.id });
+    await Group.deleteMany({ school: req.user.id });
+    await Student.deleteMany({ school: req.user.id });
+    await Lesson.deleteMany({ school: req.user.id });
+    res.status(200).json({ success: true, message: 'Account successfully deleted!' });
   } catch (e) {
     errorHandler(res, e);
   }
